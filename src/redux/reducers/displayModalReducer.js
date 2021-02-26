9cd9f675d09be48e676f46e208500b79e73b1744
Agshin Ismayilzada <aqsinismayilzada@gmail.com>
2021-02-26 22:57:21 +0400

    optimize and content updated
    Optimize images, site content uploaded

--- src/redux/reducers/displayModalReducer.js
+++ src/redux/reducers/displayModalReducer.js
@@ -1,7 +1,7 @@
 import * as actionTypes from "../actions/actionTypes";
 import initialState from "./initialState";
 
-export default function openModalReducer(state = false, action) {
+export default function displayModalReducer(state = false, action) {
   switch (action.type) {
     case actionTypes.OPEN_PORTFOLIO_MODAL:
       return action.payload;
