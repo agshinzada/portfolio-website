7c2dd6faa9fc8637c84904c390107ab84f441a25
Agshin Ismayilzada <aqsinismayilzada@gmail.com>
2021-02-28 01:49:01 +0400

    fix 13

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="portfolio">
       <React.StrictMode>
         <App />
       </React.StrictMode>
