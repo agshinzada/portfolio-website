846d1b33e3e3824c0b680bfd3cb97ee7e6711642
Agshin Ismayilzada <aqsinismayilzada@gmail.com>
2021-03-02 21:02:41 +0400

    fix

--- src/components/Utility/Buttons/DownloadBtn.js
+++ src/components/Utility/Buttons/DownloadBtn.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class downloadBtn extends Component {
   render() {
     return (
-      <a href="./resume.pdf" target="_blank">
+      <a href="./cv.pdf" target="_blank">
         <button className="btn btn--download mr-sm">Download CV</button>
       </a>
     );
